6c002cd72e63e12f85034c8476e0d807132fc0fe
vlad <vlad@vlads-MacBook-Pro.local>
2017-08-30 17:29:06 +0300

    NEED to localization, working off-line, hide label in the "searcher" when it's empty and fix cells

--- LabraFilms/AppDelegate.swift
+++ LabraFilms/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UINavigationBar.appearance().barTintColor = UIColor(red: 12/255, green: 85/255, blue: 130/255, alpha: 1)
+        UINavigationBar.appearance().barTintColor = UIColor(red: 64/255, green: 74/255, blue: 96/255, alpha: 1)
         UINavigationBar.appearance().tintColor = UIColor.white
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white]
         application.statusBarStyle = .lightContent
